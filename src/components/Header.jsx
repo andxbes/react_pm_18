d78b918f2dbf93cb20f9301a8e5a2bbf830a5564
andxbes <andxbes@gmail.com>
2024-08-13 19:45:34 +0300

    270

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logoImg from '../assets/logo.jpg';
+import Button from './UI/Button';
 
 export default function Header(params) {
     return (
@@ -8,7 +9,7 @@ export default function Header(params) {
                 <h1 >ReactFood</h1>
             </div>
             <nav>
-                <button>Card (0)</button>
+                <Button textOnly>Card (0)</Button>
             </nav>
         </header>
     );
